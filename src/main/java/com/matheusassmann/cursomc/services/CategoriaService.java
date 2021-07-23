414143bebbbfe5ab03160db8f3a47c9b2026d3ef
Matheus Assmann <matheusassmann@hotmail.com>
2021-07-23 15:47:08 -0300

    Inserting a new Categoria(POST)

--- src/main/java/com/matheusassmann/cursomc/services/CategoriaService.java
+++ src/main/java/com/matheusassmann/cursomc/services/CategoriaService.java
@@ -20,4 +20,9 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! ID: " + id + ", Tipo: " + obj.getClass().getName()));
 	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
 }
