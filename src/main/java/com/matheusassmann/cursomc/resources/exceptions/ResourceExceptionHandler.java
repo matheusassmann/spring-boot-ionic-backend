2505a87bc8df26e6d34f504179916d352ce6d1d8
Matheus Assmann <matheusassmann@hotmail.com>
2021-07-26 10:10:36 -0300

    Deleting a Categoria(DELETE)

--- src/main/java/com/matheusassmann/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/matheusassmann/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -7,14 +7,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.matheusassmann.cursomc.services.exceptions.DataIntegrityException;
 import com.matheusassmann.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
-		StandardError standardError = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		StandardError standardError = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(),
+				System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
+	}
+
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+		StandardError standardError = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardError);
 	}
 }
--- src/main/java/com/matheusassmann/cursomc/services/CategoriaService.java
+++ src/main/java/com/matheusassmann/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.matheusassmann.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.matheusassmann.cursomc.domain.Categoria;
 import com.matheusassmann.cursomc.repositories.CategoriaRepository;
+import com.matheusassmann.cursomc.services.exceptions.DataIntegrityException;
 import com.matheusassmann.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -14,20 +16,29 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! ID: " + id + ", Tipo: " + obj.getClass().getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma Categoria que possua produtos cadastrados!");
+		}
 	}
 }
